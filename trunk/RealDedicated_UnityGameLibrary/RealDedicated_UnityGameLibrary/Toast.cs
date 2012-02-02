fe07fbc8cbc59219fc6e3fef180a99284164a282
brendanerquiaga@gmail.com <brendanerquiaga@gmail.com@a226e172-ba12-4259-7538-4ca944902dd2>
2012-02-02 23:28:38 +0000

    Toast System v0.1

---
+++ trunk/RealDedicated_UnityGameLibrary/RealDedicated_UnityGameLibrary/Toast.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+
+namespace RealDedicated_UnityGameLibrary
+{
+    public class Toast : MonoBehaviour
+    {
+        #region Members
+        protected string textToDisplay;
+        protected Vector2 textPosition;
+        protected Vector2 textDirection;
+        protected float textSpeed;
+        protected GUIStyle textGUIStyle;
+
+        protected bool displayed = false;
+        protected Vector2 textSize;
+        #endregion
+
+        #region Properties
+        public string TextToDisplay
+        {
+            get { return this.textToDisplay; }
+            set { this.textToDisplay = value; }
+        }
+
+        public Vector2 TextPosition
+        {
+            get { return this.textPosition; }
+            set { this.textPosition = value; }
+        }
+
+        public Vector2 TextDirection
+        {
+            get { return this.textDirection; }
+            set { this.textDirection = value; }
+        }
+
+        public float TextSpeed
+        {
+            get { return this.textSpeed; }
+            set { this.textSpeed = value; }
+        }
+
+        public GUIStyle TextGUIStyle
+        {
+            get { return this.textGUIStyle; }
+            set { this.textGUIStyle = value; }
+        }
+
+        public Rect TextRect
+        {
+            get
+            {
+                Rect tempRect = new Rect();
+
+                tempRect.x = this.TextPosition.x;
+                tempRect.y = this.TextPosition.y;
+
+                Vector2 tempSize = GUI.skin.label.CalcSize(new GUIContent(this.TextToDisplay));
+
+                tempRect.width = tempSize.x;
+                tempRect.height = tempSize.y;
+
+                return tempRect;
+            }
+        }
+        #endregion
+
+        #region Methods
+        public void BeginDisplayingToast()
+        {
+            this.displayed = true;
+        }
+
+        public void FixedUpdate()
+        {
+            if (this.displayed)
+            {
+                this.DisplayToast();
+                this.MoveToast();
+            }
+        }
+
+        protected void DisplayToast()
+        {
+            GUI.Label(this.TextRect, new GUIContent(this.TextToDisplay), this.TextGUIStyle);
+        }
+
+        protected void MoveToast()
+        {
+            Vector2 textMoveVector = this.TextDirection * this.TextSpeed;
+
+            this.TextPosition += textMoveVector;
+        }
+        #endregion
+    }
+}
