7655a052adfe12eca575c7acd70594dbd15a6b65
brendanerquiaga@gmail.com <brendanerquiaga@gmail.com@a226e172-ba12-4259-7538-4ca944902dd2>
2012-03-21 22:45:07 +0000

    Added Referenceable Objects.

---
+++ trunk/RealDedicated_UnityGameLibrary/RealDedicated_UnityGameLibrary/General/ReferencableObjectList.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+namespace RealDedicated_UnityGameLibrary
+{
+    public class ReferencableObjectList : MonoBehaviour
+    {
+        #region Members
+        [UnityEngine.SerializeField]
+        private string objectListName;
+
+        [UnityEngine.SerializeField]
+        private ReferencableObject[] objects;
+        #endregion
+
+        #region Properties
+        public string ObjectListName
+        {
+            get { return this.objectListName; }
+            set { this.objectListName = value; }
+        }
+
+        public ReferencableObject[] GuiStyles
+        {
+            get { return this.objects; }
+            set { this.objects = value; }
+        }
+        #endregion
+
+        #region Methods
+        public void Awake()
+        {
+            this.NameObjects();
+        }
+
+        protected virtual void NameObjects()
+        {
+            for (int i = 0; i < this.objects.Length; i++)
+            {
+                if (this.objects[i].ObjectName == "")
+                {
+                    this.objects[i].ObjectName = "Object #" + i;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Retrieve Object by name 
+        /// </summary>
+        /// <param name="nameOfStyle">Name of Object</param>
+        /// <returns></returns>
+        public virtual ReferencableObject RetrieveGUIStyle(string nameOfObject)
+        {
+            ReferencableObject tempObject = new ReferencableObject();
+
+            foreach (ReferencableObject childObject in this.objects)
+            {
+                if (childObject.ObjectName == nameOfObject)
+                {
+                    tempObject = childObject;
+                    break;
+                }
+            }
+
+            return tempObject;
+        }
+        #endregion
+    }
+}
---
+++ trunk/RealDedicated_UnityGameLibrary/RealDedicated_UnityGameLibrary/General/ReferencableObjectListReference.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+
+namespace RealDedicated_UnityGameLibrary
+{
+    public class ReferencableObjectListReference : MonoBehaviour
+    {
+        #region Members
+        #region Static Instance
+        private static ReferencableObjectListReference objectListInstance = null;
+
+        public static ReferencableObjectListReference instance
+        {
+            get
+            {
+                if (objectListInstance == null)
+                {
+                    objectListInstance = FindObjectOfType(typeof(ReferencableObjectListReference)) as ReferencableObjectListReference;
+                }
+
+                if (objectListInstance == null)
+                {
+                    GameObject newObj = new GameObject("ReferencableObjectListReference");
+                    objectListInstance = newObj.AddComponent(typeof(ReferencableObjectListReference)) as ReferencableObjectListReference;
+                    Debug.Log("Could not find ReferencableObjectListReference, so I made one");
+                }
+
+                return objectListInstance;
+            }
+        }
+        #endregion
+
+        private ReferencableObjectList[] activeObjectLists;
+        #endregion
+
+        #region Properties
+        public ReferencableObjectList[] ActiveObjectLists
+        {
+            get { return this.activeObjectLists; }
+            set { this.activeObjectLists = value; }
+        }
+        #endregion
+
+        #region Methods
+        public void Awake()
+        {
+            this.GetStyleSheets();
+            this.NameStyleSheets();
+        }
+
+        protected virtual void GetStyleSheets()
+        {
+            this.activeObjectLists = FindObjectsOfType(typeof(ReferencableObjectList)) as ReferencableObjectList[];
+        }
+
+        protected virtual void NameStyleSheets()
+        {
+            for (int i = 0; i < this.activeObjectLists.Length; i++)
+            {
+                if (this.activeObjectLists[i].ObjectListName == "")
+                {
+                    this.activeObjectLists[i].ObjectListName = "Object List #" + i;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Retrieve ObjectList by name 
+        /// </summary>
+        /// <param name="nameOfStyle">Name of ObjectList</param>
+        /// <returns></returns>
+        public virtual ReferencableObjectList RetrieveObjectList(string nameOfObjectList)
+        {
+            ReferencableObjectList tempObjectList = null;
+
+            foreach (ReferencableObjectList childObjectList in this.activeObjectLists)
+            {
+                if (childObjectList.ObjectListName == nameOfObjectList)
+                {
+                    tempObjectList = childObjectList;
+                    break;
+                }
+            }
+
+            return tempObjectList;
+        }
+        #endregion
+
+    }
+}
