353604791530c6962b313eae19eeb85b64bc1748
brendanerquiaga <brendanerquiaga@gmail.com>
2012-03-26 21:40:42 +0000

    Score System Started ScoreValueSheet created CSVToScoreValueSheetParser created

---
+++ RealDedicated_UnityGameLibrary/RealDedicated_UnityGameLibrary/General/Score System/ScoreValueSheet.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace RealDedicated_UnityGameLibrary
+{
+    public class ScoreValueSheet : MonoBehaviour
+    {
+        [System.Serializable]
+        public struct ValueItem
+        {
+            public string value;
+            public float score;
+
+            public ValueItem(string newValue, float newScore)
+            {
+                value = newValue;
+                score = newScore;
+            }
+        }
+
+        #region Members
+        [UnityEngine.SerializeField]
+        private string valueSheetName;
+        [UnityEngine.SerializeField]
+        private ValueItem[] valueMap;
+        #endregion
+
+        #region Properties
+        public string ValueSheetName
+        {
+            get { return this.valueSheetName; }
+            set { this.valueSheetName = value; }
+        }
+
+        public ValueItem[] ValueMap
+        {
+            get { return this.valueMap; }
+            set { this.valueMap = value; }
+        }
+        #endregion
+    }
+}
