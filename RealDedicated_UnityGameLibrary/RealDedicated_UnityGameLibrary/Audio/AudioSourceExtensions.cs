b83a3e28ab18714e7766a4e729a44c566dd56fb8
brendanerquiaga <brendanerquiaga@gmail.com>
2012-03-28 23:39:03 +0000

    Added AudioSourceExtensions.cs PlayRandomClip PlayClip

---
+++ RealDedicated_UnityGameLibrary/RealDedicated_UnityGameLibrary/Audio/AudioSourceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace RealDedicated_UnityGameLibrary
+{
+    public static class AudioSourceExtensions
+    {
+        public static void playClip(this AudioSource audioSource, AudioClip audioClip)
+        {
+            audioSource.clip = audioClip;
+            audioSource.Play();
+        }
+
+        public static void playRandomClip(this AudioSource audioSource, AudioClip[] audioClips)
+        {
+            int clipIndex = UnityEngine.Random.Range(0, audioClips.Length);
+            audioSource.playClip(audioClips[clipIndex]);
+        }
+    }
+}
