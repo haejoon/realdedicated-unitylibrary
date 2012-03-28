fa46b15e2c03f3b6628e5beaddb145c4f9285217
brendanerquiaga@gmail.com <brendanerquiaga@gmail.com@a226e172-ba12-4259-7538-4ca944902dd2>
2012-03-28 21:44:45 +0000

    ScoreManager added

---
+++ trunk/RealDedicated_UnityGameLibrary/RealDedicated_UnityGameLibrary/General/Score System/CSVToValueMapParser.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+namespace RealDedicated_UnityGameLibrary.General.Score_System
+{
+    public class CSVToValueMapParser : MonoBehaviour
+    {
+        public TextAsset csvFile;
+        public string separator = ",";
+
+        public void Awake()
+        {
+            if (this.csvFile != null && this.separator != null)
+                this.CreateScoreValueSheet();
+        }
+
+        private void CreateScoreValueSheet()
+        {
+            ValueMap tempValueSheet = this.gameObject.AddComponent<ValueMap>();
+            tempValueSheet.ValueMapName = this.csvFile.name;
+            string[] parsedCSV = this.ParseCSV();
+
+            tempValueSheet.ValueItems = new ValueMap.ValueItem[parsedCSV.Length];
+
+            this.SetValues(parsedCSV, tempValueSheet);
+
+            Destroy(this);
+        }
+
+        private string[] ParseCSV()
+        {
+            string[] parsedCSVFile = this.csvFile.text.Split('\n');
+
+            Debug.Log(this.csvFile.text);
+
+            return parsedCSVFile;
+        }
+
+        private void SetValues(string[] parsedCSV, ValueMap valueSheet)
+        {
+            for (int i = 0; i < parsedCSV.Length; i++)
+            {
+                valueSheet.ValueItems[i] = this.GetValueItem(parsedCSV[i]);
+            }
+        }
+
+        private ValueMap.ValueItem GetValueItem(string valueString)
+        {
+            ValueMap.ValueItem tempValueItem = null;
+
+            string[] splitValueString = valueString.Split(this.separator[0]);
+            
+            float tempScore = 0;
+            float parsedFloat = 0;
+            if (float.TryParse(splitValueString[1].ToString(), out parsedFloat))
+            {
+                tempScore = parsedFloat;
+            }
+
+            tempValueItem = new ValueMap.ValueItem(splitValueString[0], tempScore);
+
+            return tempValueItem;
+        }
+
+    }
+}
---
+++ trunk/RealDedicated_UnityGameLibrary/RealDedicated_UnityGameLibrary/General/Score System/ValueMapReference.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+
+
+namespace RealDedicated_UnityGameLibrary
+{
+    public class ValueMapReference : ReferencableObjectList
+    {
+        protected override void GetObjects()
+        {
+            base.GetObjects();
+
+            ValueMap[] tempScoreSheets = this.gameObject.GetComponents<ValueMap>();
+            ReferencableObject[] tempRefObjects = new ReferencableObject[tempScoreSheets.Length];
+            Debug.Log("I found: " + tempScoreSheets.Length + " Score Sheets");
+
+            for(int i = 0; i < tempRefObjects.Length; i++)
+            {
+                Debug.Log("Score sheet: " + i);
+                tempRefObjects[i] = new ReferencableObject();
+                tempRefObjects[i].ObjectName = tempScoreSheets[i].ValueMapName;
+                tempRefObjects[i].ObjectToReference = tempScoreSheets[i];
+            }
+
+            this.Objects = new ReferencableObject[tempRefObjects.Length];
+            this.Objects = tempRefObjects;
+        }
+
+        protected override void NameObjects()
+        {
+            for (int i = 0; i < this.Objects.Length; i++)
+            {
+                string scoreSheetName = (this.Objects[i].ObjectToReference as ValueMap).ValueMapName;
+
+                if (scoreSheetName != "")
+                {
+                    this.Objects[i].ObjectName = scoreSheetName;
+                }
+            }
+
+            base.NameObjects();
+        }
+
+        /// <summary>
+        /// Returns a score from a connected value. 
+        /// </summary>
+        /// <param name="connectedValue"></param>
+        /// <returns></returns>
+        protected virtual float GetScoreFromValue(string connectedValue)
+        {
+            foreach (ReferencableObject childObject in this.Objects)
+            {
+                ValueMap.ValueItem[] valueMap = (childObject.ObjectToReference as ValueMap).ValueItems;
+
+                foreach (ValueMap.ValueItem childValueItem in valueMap)
+                {
+                    if (childValueItem.value == connectedValue)
+                    {
+                        return childValueItem.score;
+                    }
+                }
+            }
+
+            return 0;
+        }
+
+        /// <summary>
+        /// Returns a score from a connected value from a specific ScoreSheet
+        /// </summary>
+        /// <param name="connectedValue"></param>
+        /// <returns></returns>
+        protected virtual float GetScoreFromValue(string connectedValue, string scoreSheetName)
+        {
+            foreach (ReferencableObject childObject in this.Objects)
+            {
+                if ((childObject.ObjectToReference as ValueMap).ValueMapName == scoreSheetName)
+                {
+                    ValueMap.ValueItem[] valueMap = (childObject.ObjectToReference as ValueMap).ValueItems;
+
+                    foreach (ValueMap.ValueItem childValueItem in valueMap)
+                    {
+                        if (childValueItem.value == connectedValue)
+                        {
+                            return childValueItem.score;
+                        }
+                    }
+                }
+            }
+
+            return 0;
+        }
+    }
+}
