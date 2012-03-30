b638b0e7e11daed6c367771fb24f3126ef9371f0
brendanerquiaga@gmail.com <brendanerquiaga@gmail.com@a226e172-ba12-4259-7538-4ca944902dd2>
2012-03-30 20:38:11 +0000

    Added onComplete action to playClip & playRandomClip Extension Methods Added OneShotAudioPlayer.cs

--- trunk/RealDedicated_UnityGameLibrary/RealDedicated_UnityGameLibrary/Audio/AudioSourceExtensions.cs
+++ trunk/RealDedicated_UnityGameLibrary/RealDedicated_UnityGameLibrary/Audio/AudioSourceExtensions.cs
@@ -1,4 +1,7 @@
 ﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
 
 namespace RealDedicated_UnityGameLibrary
 {
@@ -10,10 +13,31 @@ namespace RealDedicated_UnityGameLibrary
             audioSource.Play();
         }
 
+        public static IEnumerator playClip(this AudioSource audioSource, AudioClip audioClip, Action onComplete)
+        {
+            audioSource.playClip(audioClip);
+
+            while (audioSource.isPlaying)
+                yield return null;
+
+            onComplete();
+        }
+
         public static void playRandomClip(this AudioSource audioSource, AudioClip[] audioClips)
         {
             int clipIndex = UnityEngine.Random.Range(0, audioClips.Length);
             audioSource.playClip(audioClips[clipIndex]);
+        }
+
+        public static IEnumerator playRandomClip(this AudioSource audioSource, AudioClip[] audioClips, Action onComplete)
+        {
+            int clipIndex = UnityEngine.Random.Range(0, audioClips.Length);
+            audioSource.playClip(audioClips[clipIndex]);
+
+            while (audioSource.isPlaying)
+                yield return null;
+
+            onComplete();
         }
     }
 }
